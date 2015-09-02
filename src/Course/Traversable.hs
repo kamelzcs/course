4d67c102aa24d31c9115e856b1ba37d294123534
kamelzcs <kamelzcs@gmail.com>
2015-09-02 17:43:23 +0900

    added traversable

--- src/Course/Traversable.hs
+++ src/Course/Traversable.hs
@@ -34,4 +34,4 @@ instance Traversable List where
     -> List a
     -> f (List b)
   traverse f =
-    foldRight (\a b -> (:.) <$> f a <*> b) (pure Nil)
+    foldRight (\a b -> lift2 (:.) (f a) b) (pure Nil)
